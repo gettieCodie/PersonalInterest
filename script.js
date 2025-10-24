a265891c7c79144f03b4a68e21c99f59a2699d8e
Gertrude Kristine Agena <saucegettie@gmail.com>
2025-10-24 11:49:54 +0800

    added few tweaks

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener('click', () => {
       if (button.classList.contains('active')) return;
 
-      // deactivate previous
+
       tabButtons.forEach(btn => btn.classList.remove('active'));
       tabContents.forEach(content => content.classList.remove('active'));
 
-      // activate new
+
       button.classList.add('active');
       const target = document.getElementById(button.dataset.tab);
       target.classList.add('active');
@@ -18,15 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// Duplicate brands for infinite scroll
 document.addEventListener('DOMContentLoaded', function() {
   const scrollerInner = document.querySelector('.scroller__inner');
   const brands = scrollerInner.innerHTML;
-  
-  // Duplicate content 2 times for seamless loop
+
   scrollerInner.innerHTML = brands + brands + brands;
   
-  // Enable animation
   document.querySelector('.scroller').setAttribute('data-animated', 'true');
 });
 
